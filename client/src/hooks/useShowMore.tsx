4f0f32338ee3814f6d43b68a60329bf60d75589d
yusufyilmz <yusufyilmz@yusufs-MacBook-Pro.local>
2020-08-11 17:46:52 +0200

    fix

--- client/src/hooks/useShowMore.tsx
+++ client/src/hooks/useShowMore.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { Button } from "react-bootstrap";
 
+const perItemsInPage = 6;
 const useShowMore = (items: Array<unknown>) => {
 
     const [listitems, setItems] = useState<any>([]);
@@ -9,7 +10,7 @@ const useShowMore = (items: Array<unknown>) => {
 
     const populateList = useCallback(
         (count) => {
-            const arr = [...items.slice(0, count + 5)];
+            const arr = [...items.slice(0, count + perItemsInPage)];
             // eslint-disable-next-line @typescript-eslint/no-unused-expressions
             items.length > arr.length ? setShowMore(true) : setShowMore(false);
          
@@ -30,7 +31,7 @@ const useShowMore = (items: Array<unknown>) => {
                 style={{ width: '100%'}}
                 key="button"
                 variant="info"
-                onClick={() => setLastCount(i  => i + 5)}>
+                onClick={() => setLastCount(i  => i + perItemsInPage)}>
                 Show more</Button>
         )
     }
